3a2db48f2306369f2169401ec74e92f9fc97efe7
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-02 15:18:29 -0400

    Avancement JWT opérationnel

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -1,8 +1,41 @@
 'use strict';
 angular.module('tpApp')
-  .controller('TPLoginCtrl', function ($scope, $http) {
+  .controller('TPLoginCtrl', function ($scope, $http, $rootScope) {
     $scope.hideSuccess = true;
 
     $scope.user = [ {email: null},
                     {password: null}];
+
+    $scope.messageServeur = [{message: null}];
+    $scope.loginMessageBoxColor = "";
+    $scope.btnSubmit = function(){
+      $http({   method: 'POST',
+        url: 'http://crispesh.herokuapp.com/api/login_check',
+        data: {username: $scope.user.email,
+              password: $scope.user.password}
+      })
+        .then(
+          function successCallback(data) {
+            $scope.messageServeur = [{message: null}];
+            $scope.messageServeur.push({message: "Connexion réussis"});
+            $scope.messageServeur.push({message: null});
+            $scope.loginMessageBoxColor = "alert-success";
+            $scope.hideSuccess = false;
+            $rootScope.username = $scope.user.email;
+            $rootScope.$broadcast('user:logged',{});
+            localStorage.setItem('JWT', data.data.token);
+            console.log("Saving token to localstorage", data.data.token);
+          },
+          function errorCallback(response){
+            $scope.loginMessageBoxColor = "alert-danger";
+            $scope.messageServeur = [{message: null}];
+            $scope.messageServeur.push({message: response.data.code + " " + response.data.message});
+            $scope.messageServeur.push({message: null});
+            angular.forEach(response.data.errors, function(value, key){
+              $scope.messageServeur.push({message: value.property_path.concat(" : ").concat(value.message)});
+            });
+            $scope.messageServeur.push({message: null});
+          }
+        )
+    };
   });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,39 +1,73 @@
 'use strict';
 
-class NavbarController {
+//class NavbarController {
+angular.module('tpApp')
+  .controller('NavbarController', function ($scope, $http, $rootScope) {
   //start-non-standard
-  menu = [{
-      'title': 'Accueil',
-      'state': 'main'
-    },
+    var jwt = localStorage.getItem('JWT');
+    if(jwt === null)
     {
-      'title': 'Recherche',
-      'state': 'search'
-    },
-    {
-      'title': 'Liste de Film',
-      'state': 'playList'
-    },
-    {
-      'title': 'Contact',
-      'state': 'about'
-    },
-    {
-      'title': 'Inscription',
-      'state': 'registration'
-    },
-    {
-      'title': 'Connexion',
-      'state': 'login'
-    },
-  ];
+      $scope.menu = [{
+        'title': 'Accueil',
+        'state': 'main'
+      },
+        {
+          'title': 'Recherche',
+          'state': 'search'
+        },
+        {
+          'title': 'Contact',
+          'state': 'about'
+        },
+        {
+          'title': 'Inscription',
+          'state': 'registration'
+        },
+        {
+          'title': 'Connexion',
+          'state': 'login'
+        },
+      ];
+    }
+    else{
+      $scope.menu = [{
+        'title': 'Accueil',
+        'state': 'main'
+      },
+        {
+          'title': 'Recherche',
+          'state': 'search'
+        },
+        {
+          'title': 'Liste de Film',
+          'state': 'playList'
+        },
+        {
+          'title': 'Contact',
+          'state': 'about'
+        },
+        {
+          'title': $rootScope.username,
+          'state': 'login'
+        },
+        {
+          'title': 'Déconnexion',
+          'state': 'main'
+        },
+      ];
+    }
 
-  isCollapsed = true;
+   /* $scope.logout = function(){
+      $rootScope.$broadcast('user:logout', {});
+      localstorage.removeItem('JWT');
+    }*/
+
+  $scope.isCollapsed = true;
   //end-non-standard
 
-  constructor() {
-    }
-}
+  /*constructor() {
+    }*/
+});
 
-angular.module('tpApp')
-  .controller('NavbarController', NavbarController);
+//angular.module('tpApp')
+  //.controller('NavbarController', NavbarController);
