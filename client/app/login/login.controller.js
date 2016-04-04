117b4ef26c3249becfae0c5db4ab4ab3fddd34c0
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-03 21:29:53 -0400

    ajout des test pour Login 3 qui échou

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -8,7 +8,7 @@ angular.module('tpApp')
 
     $scope.messageServeur = [{message: null}];
     $scope.loginMessageBoxColor = "";
-    $scope.btnSubmit = function(){
+    $scope.btnConnect = function(){
       $http({   method: 'POST',
         url: 'http://crispesh.herokuapp.com/api/login_check',
         data: {username: $scope.user.email,
--- e2e/login/login.spec.js
+++ e2e/login/login.spec.js
@@ -2,7 +2,7 @@
 
 var config = browser.params;
 
-describe('Registration View', function() {
+describe('Login View', function() {
   var page;
 
   beforeEach(function() {
@@ -21,6 +21,34 @@ describe('Registration View', function() {
     expect(element(by.model('user.email')).isPresent());
   });
 
+  it("Test de connexion Réussis", function() {
+    element(by.model('user.email')).sendKeys('Eti@test2.com');
+    element(by.model('user.password')).sendKeys('asd');
+    element(by.id('btnConnect')).click();
+    expect(browser.get("/main"));
+  });
+
+  it("Test de connexion Échouer, le mots de passe n'est pas bon", function() {
+    element(by.model('user.email')).sendKeys('Eti@test2.com');
+    element(by.model('user.password')).sendKeys('asdf');
+    element(by.id('btnConnect')).click();
+    expect(element(by.id('registrationMessage')).getText()).toBe('401 Bad credentials');
+  });
+
+  it("Test de connexion Échouer, le courriel n'est pas bon", function() {
+    element(by.model('user.email')).sendKeys('Eti@tes.com');
+    element(by.model('user.password')).sendKeys('asd');
+    element(by.id('btnConnect')).click();
+    expect(element(by.id('registrationMessage')).getText()).toBe('401 Bad credentials');
+  });
+
+  it("Test de connexion Échouer, tout les données son érroner", function() {
+    element(by.model('user.email')).sendKeys('Eti@tes.com');
+    element(by.model('user.password')).sendKeys('asdf');
+    element(by.id('btnConnect')).click();
+    expect(element(by.id('registrationMessage')).getText()).toBe('401 Bad credentials');
+  });
+
   it("Regarder si la page contient le champ pour entrer le mot de passe", function() {
     expect(element(by.id('inputUserPassword')).isPresent());
   });
@@ -30,7 +58,7 @@ describe('Registration View', function() {
   });
 
   it("Regarder si la page contient le bouton Envoyer", function() {
-    expect(element(by.id('btnSend')).isPresent());
+    expect(element(by.id('btnConnect')).isPresent());
   });
 
   it("Regarder si la page contient le bouton Effacer", function() {
