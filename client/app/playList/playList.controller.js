33af189766ac7e81bd13fe5b507c4c8b3e8bf91c
Frédéric Bérubé <fredob@hotmail.ca>
2016-04-05 18:45:57 -0400

    Why ?

--- client/app/playList/playList.controller.js
+++ client/app/playList/playList.controller.js
@@ -1,6 +1,30 @@
 'use strict';
 
 angular.module('tpApp')
-  .controller('PlayListCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PlayListCtrl', function ($scope, $resource, $http, Favoris) {
+
+      $scope.favorites = {};
+      $scope.messageServeur = '';
+
+      $scope.getAllFav = function(){
+          $http({
+              method: 'GET',
+              url: 'http://crispesh.herokuapp.com/api/me',
+              timeout: 5000
+          })
+              .then(
+                  function successCallback(response){
+                      console.log(Hello);
+                      $scope.favorites = response.data.Search;
+                  }
+              ),
+              function errorCallback(response) {
+                  console.log(allo);
+                  $scope.messageServeur = 'Erreur Serveur';
+              }
+      }
+
+      $scope.favDelete = function(){
+          Favoris.delete({})
+      }
   });
