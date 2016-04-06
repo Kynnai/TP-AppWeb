6b97c79420bd4240fadc017d05e9a6afd5103e98
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-06 19:28:22 -0400

    correction des tests

--- client/app/logout/logout.controller.js
+++ client/app/logout/logout.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
 angular.module('tpApp')
-  .controller('TPLogoutCtrl', function ($scope) {
-    localStorage.removeItem('JWT');
-    localStorage.removeItem('USER');
-    location.href="/main"
+  .controller('TPLogoutCtrl', function () {
+
+    var isOk = false;
+    var jwt = localStorage.getItem('JWT');
+    var user = localStorage.getItem('USER');
+
+    if(jwt !== null){
+      localStorage.removeItem('JWT');
+      isOk = true;
+    }
+
+    if(user !== null){
+      localStorage.removeItem('USER');
+      isOk = true;
+    }
+
+    if(isOk){
+      location.href = "/main"
+    }
   });
--- e2e/login/login.spec.js
+++ e2e/login/login.spec.js
@@ -3,7 +3,6 @@
 var config = browser.params;
 
 describe('Login View', function() {
-  var page;
 
   beforeEach(function() {
     browser.get("/login");
@@ -74,89 +73,89 @@ describe('Login View', function() {
     expect(element(by.id('logout')).getText()).toBe('Déconnexion');
   });
 
-  it("16. Connexion Réussis l'onglet Connexion disparait de la nav-bar", function() {
+  it("15. Connexion Réussis l'onglet Connexion disparait de la nav-bar", function() {
     element(by.model('user.email')).sendKeys('Eti@test2.com');
     element(by.model('user.password')).sendKeys('asd');
     element(by.id('btnConnect')).click();
     expect(element(by.id('login')).isPresent()).toBe(false);
   });
 
-  it("17. Connexion Réussis l'onglet Inscription disparait de la nav-bar", function() {
+  it("16. Connexion Réussis l'onglet Inscription disparait de la nav-bar", function() {
     element(by.model('user.email')).sendKeys('Eti@test2.com');
     element(by.model('user.password')).sendKeys('asd');
     element(by.id('btnConnect')).click();
     expect(element(by.id('registration')).isPresent()).toBe(false);
   });
 
-  it("18. Connexion Réussis redirection vers la page d'accueil", function() {
+  it("17. Connexion Réussis redirection vers la page d'accueil", function() {
     element(by.model('user.email')).sendKeys('Eti@test2.com');
     element(by.model('user.password')).sendKeys('asd');
     element(by.id('btnConnect')).click();
     expect(element(by.id('banner')).getText()).toBe("Six films de l'année 2016 !");
   });
 
-  it("19. Connexion Échouer, le mots de passe n'est pas bon", function() {
+  it("18. Connexion Échouer, le mots de passe n'est pas bon", function() {
     element(by.model('user.email')).sendKeys('Eti@test2.com');
     element(by.model('user.password')).sendKeys('asdf');
     element(by.id('btnConnect')).click();
     expect(element(by.id('registrationMessage')).getText()).toBe('401 Bad credentials');
   });
 
-  it("20. Connexion Échouer, le courriel n'est pas bon", function() {
+  it("19. Connexion Échouer, le courriel n'est pas bon", function() {
     element(by.model('user.email')).sendKeys('Eti@tes.com');
     element(by.model('user.password')).sendKeys('asd');
     element(by.id('btnConnect')).click();
     expect(element(by.id('registrationMessage')).getText()).toBe('401 Bad credentials');
   });
 
-  it("21. Connexion Échouer, tout les données son érroner", function() {
+  it("20. Connexion Échouer, tout les données son érroner", function() {
     element(by.model('user.email')).sendKeys('Eti@tes.com');
     element(by.model('user.password')).sendKeys('asdf');
     element(by.id('btnConnect')).click();
     expect(element(by.id('registrationMessage')).getText()).toBe('401 Bad credentials');
   });
 
-  it("22. Regarder si la page contient le champ pour entrer le mot de passe", function() {
+  it("21. Regarder si la page contient le champ pour entrer le mot de passe", function() {
     expect(element(by.id('inputUserPassword')).isDisplayed()).toBeTruthy();
   });
 
-  it("23. Regarder si la page contient le model du champ pour entrer le mot de passe", function() {
+  it("22. Regarder si la page contient le model du champ pour entrer le mot de passe", function() {
     expect(element(by.model('user.password')).isDisplayed()).toBeTruthy();
   });
 
-  it("24. Regarder si le message d'erreur pour le Courriel n'est pas visible", function() {
+  it("23. Regarder si le message d'erreur pour le Courriel n'est pas visible", function() {
     expect(element(by.id('errorMessageEmail')).isDisplayed()).toBeFalsy();
   });
 
-  it("25. Regarder si le message pour le Courriel fonctionne lorsqu'on efface le courriel", function() {
+  it("24. Regarder si le message pour le Courriel fonctionne lorsqu'on efface le courriel", function() {
     element(by.model('user.email')).sendKeys('Eti@test.com');
     element(by.model('user.email')).clear();
     expect(element(by.id('errorMessageEmail')).isDisplayed()).toBeTruthy();
   });
 
-  it("26. Regarder si le message d'erreur pour le mot de passe n'est pas visible", function() {
+  it("25. Regarder si le message d'erreur pour le mot de passe n'est pas visible", function() {
     expect(element(by.id('errorMessagePassword')).isDisplayed()).toBeFalsy();
   });
 
-  it("27. Regarder si le message pour le Courriel fonctionne lorsqu'on efface le mot de passe", function() {
+  it("26. Regarder si le message pour le Courriel fonctionne lorsqu'on efface le mot de passe", function() {
     element(by.model('user.password')).sendKeys('asdf');
     element(by.model('user.password')).clear();
     expect(element(by.id('errorMessagePassword')).isDisplayed()).toBeTruthy();
   });
 
-  it("28. Regarder si la page contient le bouton Envoyer", function() {
+  it("27. Regarder si la page contient le bouton Envoyer", function() {
     expect(element(by.id('btnConnect')).isDisplayed()).toBeTruthy();
   });
 
-  it("29. Regarder si le bouton Connecté est désactivé", function() {
+  it("28. Regarder si le bouton Connecté est désactivé", function() {
     expect(element(by.id('btnConnect')).isDisabled);
   });
 
-  it("30. Regarder si la page contient le bouton Effacer", function() {
+  it("29. Regarder si la page contient le bouton Effacer", function() {
     expect(element(by.id('btnErase')).isDisplayed()).toBeTruthy();
   });
 
-  it("31. Regarder si le bouton Effacer fonctionne", function() {
+  it("30. Regarder si le bouton Effacer fonctionne", function() {
     element(by.model('user.email')).sendKeys('Eti@tes.com');
     element(by.model('user.password')).sendKeys('asdf');
     element(by.id('btnErase')).click();
