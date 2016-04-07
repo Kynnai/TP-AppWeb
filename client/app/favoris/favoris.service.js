3c4325aab9cf2165ef642174dd8acfba650b7923
Frédéric Bérubé <fredob@hotmail.ca>
2016-04-07 17:31:35 -0400

    Ajout du service 'Favoris'

---
+++ client/app/favoris/favoris.service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var myApp = angular.module('tpApp');
+
+myApp.service('Favoris', function ($resource) {
+  var restAPIUrl = 'https://crispesh.herokuapp.com/api';
+  return $resource(restAPIUrl + '/favs/:movie_id', { 'movie_id': '@movieId' }, { update: { method: 'PUT' } });
+});
+//# sourceMappingURL=favoris.service.js.map
