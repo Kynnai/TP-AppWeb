fe53e758b34ffd1ceb2ef1728e2996d9932ddaff
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-04-07 15:31:33 -0400

    a

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tpApp')
-  .controller('TPSearchCtrl', function ($scope, $http, Favoris) {
+  .controller('TPSearchCtrl', function ($scope, $http, Favoris, $root) {
 
     $scope.movies = {};
     $scope.query ='';
