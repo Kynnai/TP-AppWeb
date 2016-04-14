40bcdd019ca3b28afd76700ad955985569f7e5f1
Frédéric Bérubé <fredob@hotmail.ca>
2016-04-14 16:24:25 -0400

    Création de la fonction  mySocket.on()

--- client/app/chat/chat.controller.js
+++ client/app/chat/chat.controller.js
@@ -3,9 +3,14 @@
 angular.module('tpApp')
   .controller('ChatCtrl', function ($scope, mySocket) {
 
+    $scope.chatMessages = [];
+
     $scope.btnSend = function(){
       mySocket.emit('chat message', $scope.body);
       $scope.body = '';
-    }
+    };
 
+    mySocket.on('chat message', function(message){
+      $scope.chatMessages.push(message);
+    })
   });
