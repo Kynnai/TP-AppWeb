a62c6dcb84cb92469cc68f3ecc239fc7bf727ca8
Frédéric Bérubé <fredob@hotmail.ca>
2016-04-14 16:13:38 -0400

    Commit function btnSend

--- client/app/chat/chat.controller.js
+++ client/app/chat/chat.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('tpApp')
-  .controller('ChatCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ChatCtrl', function ($scope, mySocket) {
+
+    $scope.btnSend = function(){
+      mySocket.emit('chat message', $scope.body);
+      $scope.body = '';
+    }
+
   });
