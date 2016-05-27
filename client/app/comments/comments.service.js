bc9e0f0772b6f3960227c92ea9d5438198a1ce67
Frédéric Bérubé <fredob@hotmail.ca>
2016-05-27 14:17:39 -0400

    Minification surement inutile

--- client/app/comments/comments.service.js
+++ client/app/comments/comments.service.js
@@ -2,7 +2,7 @@
 
 var commentService = angular.module('tpApp');
 
-commentService.service('Comments', function ($resource) {
+commentService.service('Comments', ['$resource', function ($resource) {
   var restAPIUrl = 'https://appxapi.herokuapp.com/api';
   return $resource(restAPIUrl + '/comments/:movie_id', { 'movie_id': '@movieId' }, { update: { method: 'PUT' } });
-});
+}]);
--- client/app/favoris/favoris.service.js
+++ client/app/favoris/favoris.service.js
@@ -2,8 +2,8 @@
 
 var myApp = angular.module('tpApp');
 
-myApp.service('Favoris', function ($resource) {
+myApp.service('Favoris', ['$resource', function ($resource) {
   var restAPIUrl = 'https://appxapi.herokuapp.com/api/favs';
   return $resource(restAPIUrl + '/:id', { 'id': '@id' }, { update: { method: 'PUT' } });
-});
+}]);
 //# sourceMappingURL=favoris.service.js.map
